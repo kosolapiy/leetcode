f222e45934f3059721d40605c4cf8ca776b1fd98
kosolapiy <kosolapiy@bk.ru>
2019-03-04 14:58:42 +0300

    commit

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,8 +1,10 @@
 import array.SingleNumber;
+import strings.ImplementStrStr;
 
 public class Solution {
     public static void main(String[] args) {
         int[] nums = {4,1,2,1,2};
-        System.out.println(new SingleNumber().singleNumber(nums));
+        String haystack = "hello", needle = "ll";
+        System.out.println(new ImplementStrStr().strStr(haystack, needle));
     }
 }
